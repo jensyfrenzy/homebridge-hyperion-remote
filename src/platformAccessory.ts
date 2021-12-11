245b58b4246f9f4bc50d51c555ebf9787292af6a
jens <jens.clayson@formstack.com>
2021-12-11 12:51:09 -0600

    added more logging

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -83,6 +83,7 @@ export class platformAccessory {
     };
     const response = await axios.post(this.requestUrl, JSON.stringify(requestBody));
     const data = response.data;
+    this.platform.log.log(LogLevel.INFO, 'Response from getOn: ', data);
     this.isOn = data.info?.activeEffects[0]?.name === this.platform.config.hyperionEffect;
     return this.isOn;
   }
